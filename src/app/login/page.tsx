3eeabb8960ec7b990e1dcebb79d847c963353ced
Ram Murmu <runashInc@outlook.com>
2025-08-27 00:40:14 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/landing-header";
-import LoginForm from "@/components/auth/login-form";
+{/* import LoginForm from "@/components/auth/login-form"; */}
 import Footer from "@/components/landing-footer";
 
 export default function LoginPage() {
@@ -7,7 +7,7 @@ export default function LoginPage() {
     <div className="min-h-screen bg-black text-white relative">
       <Header />
       <main className="lg:pt-48">
-        <LoginForm />
+        
       </main>
       <Footer />
     </div>
