97d3a339d0e76f1cd92f198754cbbcc091b0d921
Ram Murmu <runashInc@outlook.com>
2025-08-27 00:51:58 +0530

    Create page.tsx
    Add an signin page

---
+++ src/app/app/signin/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/landing-header";
+import Footer from "@/components/landing-footer";
+
+export default function SigninPage() {
+  return (
+    <div className="min-h-screen bg-black text-white relative">
+      <Header />
+      <main className="lg:pt-48">
+     
+      </main>
+      <Footer />
+    </div>
+  );
+}
