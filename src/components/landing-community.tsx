707fa4a6b9a8a47c5403297149f0d03942c88f5b
Ram Murmu <runashInc@outlook.com>
2025-09-08 13:04:59 +0530

    Update landing-community.tsx

--- src/components/landing-community.tsx
+++ src/components/landing-community.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
 import { Github, MessageCircle, Users, Star, GitFork, Eye, Twitter, DiscIcon as Discord } from "lucide-react"
 import Link from "next/link"
 
