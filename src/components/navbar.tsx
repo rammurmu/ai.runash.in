158a6785879877d713eb10a30e80cd15202dc62b
Ram Murmu <runashInc@outlook.com>
2025-10-07 15:57:57 +0530

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -16,7 +16,7 @@ export default function Navbar() {
   ];
 
   return (
-    <header className="w-full fixed top-0 left-4 z-50 bg-white/80 dark:bg-black/80 backdrop-blur border-b border-gray-200 dark:border-gray-800 flex items-center justify-between px-6 h-16">
+    <header className="w-full fixed top-0 left-6 z-50 bg-white/80 dark:bg-black/80 backdrop-blur border-b border-gray-200 dark:border-gray-800 flex items-center justify-between px-6 h-16">
       <Link href="/index" className="font-bold text-xl">
         Runash AI
       </Link>
